9804655407ef063f6cfd77af8ed15a3735192314
sergio falcetti <sergio.falcetti@codenapp.com>
2023-08-23 11:27:50 -0300

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,46 @@
-import logo from './assets/logo.svg'
+import logo from "./assets/logo.svg";
 
 function App() {
-
-
   return (
     <>
-      <div className='container'>
-      <header className="header">
-        <img className="logo" src={logo} alt="" />
-        <span>Favor entrar com suas informações.</span>
-      </header>
-      <form>
-        <div className="inputContainer">
-        <label htmlFor="email">E-mail</label>
-        <input type="text" name="email" id="email" placeholder="sergio@email.com"/>
+      <div className="geral">
+        <div className="container">
+          <header className="header">
+            <span>Favor entrar com suas informações.</span>
+          </header>
+          <form>
+            <div className="inputContainer">
+              <label htmlFor="email">E-mail</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="sergio@email.com"
+              />
+            </div>
+            <div className="inputContainer">
+              <label htmlFor="senha">Senha</label>
+              <input
+                type="senha"
+                name="senha"
+                id="senha"
+                placeholder="************"
+              />
+            </div>
+            <a href="">Esqueceu sua senha?</a>
+            <button className="button">Entrar</button>
+            <div className="footer">
+              <p>Não tem uma conta?</p>
+              <a href="">Criar uma conta</a>
+            </div>
+          </form>
         </div>
-        <div className="inputContainer">
-        <label htmlFor="senha">Senha</label>
-        <input type="senha" name="senha" id="senha" placeholder="************"/>
+        <div className="direita"> 
+         <img className="logo" src={logo} alt="" />
         </div>
-
-        <a href="">Esqueceu sua senha?</a>
-
-        <button className="button">Entrar</button>
-        <div className="footer">
-          <p>Não tem uma conta?</p>
-          <a href="">Criar uma conta</a>
-        </div>
-      </form>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
